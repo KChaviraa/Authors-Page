ad11d664245d8b3961b5bd5993acb6a2cc095db2
Kate Chavira <143099677+KChaviraa@users.noreply.github.com>
2024-05-03 13:10:51 -0500

    Adding buttons, styling, and error messages to authors display page

--- script.js
+++ script.js
@@ -6,19 +6,6 @@ let startingIndex = 0;
 let endingIndex = 8;
 let authorDataArr = [];
 
-////function to populate the UI
-const displayAuthors = (authors) => {
-    authors.forEach(({author, image, url, bio}, index) => {
-    authorContainer.innerHTML += 
-    `<div id="${index}" class = "user-card">
-    <h2 class = "author-name">${author}</h2>
-    <img class = "user-img" src = "${image}" alt ="${author} avatar" >
-    <p class = "bio">${bio}</p>
-    <a class= "author-link" href="${url}" target = "_blank">${author}'s author page</a>
-    </div>`;
- });
-};
-
 //Make a GET request
 fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
 .then((res) => res.json())
@@ -27,5 +14,39 @@ fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
     displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
 
 })
-.catch((err) => console.error(`There was an error: ${err}`));
+.catch((err) => { 
+    authorContainer.innerHTML = '<p class = "error-msg">There was an error loading the authors</p>';
+  });
+
+//function to fetch more Authors
+const fetchMoreAuthors = () => {
+    startingIndex += 8;
+    endingIndex += 8;
+    displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
+    if(authorDataArr.length <= endingIndex) {
+        loadMoreBtn.disabled = true;
+        loadMoreBtn.style.cursor = "not-allowed"
+        loadMoreBtn.textContent = "No more data to load";   
+    }
+}
+
+////function to populate the UI
+const displayAuthors = (authors) => {
+    authors.forEach(({author, image, url, bio}, index) => {
+    authorContainer.innerHTML += 
+    `<div id="${index}" class = "user-card">
+    <h2 class = "author-name">${author}</h2>
+    <img class = "user-img" src = "${image}" alt ="${author} avatar" >
+    <div class="purple-divider"></div>    
+    <p class="bio">${bio.length > 50 ? bio.slice(0, 50) + "..." : bio}</p>
+    <a class= "author-link" href="${url}" target = "_blank">${author}'s author page</a>
+    </div>`;
+ });
+};
+
+loadMoreBtn.addEventListener("click", fetchMoreAuthors);
+
+
+
+
 
